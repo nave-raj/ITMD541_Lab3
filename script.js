4f090729b5f6d7aff360f5f169c1cae1d95a9ac1
Naveena Kandasamy Rajkumar <129906985+nave-raj@users.noreply.github.com>
2023-10-29 14:12:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function () {
     function calculateTotalBill () {
         let billTotal = document.getElementById('billTotal').value;
         const regex = /^[0-9]+$/;
-        if((!billTotal.match(regex)) && billTotal.length > 0){
+        if((!billTotal.match(regex))&& billTotal.length > 0){
             alert("Please Enter a Valid Bill Amount");
             document.getElementById('tip').value = 0;
             document.getElementById('tipPercentage').value = "";
@@ -20,12 +20,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const tip = parseFloat(document.getElementById('tip').value);
         const tipAmount = (billTotal * tip) / 100;
         const totalBillWithTip = billTotal + tipAmount;
-
-        if((billTotal < 0 || billTotal == null)){
-            alert("Please Enter a Valid Bill Amount");
-           
-        }   
-
+``
         document.getElementById('tipPercentage').value = tip.toFixed(2);
         document.getElementById('tipAmount').value = tipAmount.toFixed(2);
         document.getElementById('totalBillWithTip').value = totalBillWithTip.toFixed(2);
